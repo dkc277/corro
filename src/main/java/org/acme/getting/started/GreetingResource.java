1ec9d011a162a1a963c46b2b10043128ab218c85
Kunal Chaturvedi <kchaturv@gmail.com>
2021-06-05 23:49:30 +1000

    report working

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -1,7 +1,8 @@
 package org.acme.getting.started;
 
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,29 +15,29 @@ import javax.ws.rs.core.MediaType;
 import org.eclipse.birt.core.exception.BirtException;
 import org.eclipse.birt.core.framework.Platform;
 import org.eclipse.birt.report.engine.api.EngineConfig;
-import org.eclipse.birt.report.engine.api.EngineConstants;
 import org.eclipse.birt.report.engine.api.EngineException;
-import org.eclipse.birt.report.engine.api.IRenderOption;
 import org.eclipse.birt.report.engine.api.IReportEngine;
 import org.eclipse.birt.report.engine.api.IReportEngineFactory;
 import org.eclipse.birt.report.engine.api.IReportRunnable;
 import org.eclipse.birt.report.engine.api.IRunAndRenderTask;
 import org.eclipse.birt.report.engine.api.PDFRenderOption;
-import org.eclipse.birt.report.engine.api.RenderOption;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/hello")
 public class GreetingResource {
+
+    @ConfigProperty(name = "birt.report")
+    String reportPath;
 
     private IReportEngine birtEngine;
     private Map<String, IReportRunnable> reports = new HashMap<>();
 
     @PostConstruct
-    public void before() throws BirtException {
+    public void before() throws BirtException, FileNotFoundException {
         EngineConfig config = new EngineConfig();
         try {
             Platform.startup(config);
         } catch (BirtException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         IReportEngineFactory factory = (IReportEngineFactory) Platform
@@ -45,64 +46,29 @@ public class GreetingResource {
         loadReports();
     }
 
-    public void loadReports() throws EngineException {
-        File file = new File("new_report.rptdesign");
-        reports.put("new_report", birtEngine.openReportDesign(file.getAbsolutePath()));
+    public void loadReports() throws EngineException, FileNotFoundException {
+        File file = new File(this.getClass().getClassLoader()
+            .getResource(reportPath + File.separatorChar + "report-1.rptdesign").getFile());
+        System.out.println(new File(this.getClass().getClassLoader()
+            .getResource(reportPath + File.separatorChar + "customerData.csv").getPath()));
 
+        reports.put("report-1", birtEngine.openReportDesign(new FileInputStream(file)));
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        generatePDFReport(this.reports.get("new_report"));
+    public String hello() throws EngineException {
+        generatePDFReport(this.reports.get("report-1"));
         return "Hello RESTEasy";
     }
 
-    private void generatePDFReport(IReportRunnable report) {
+    private void generatePDFReport(IReportRunnable report) throws EngineException {
         IRunAndRenderTask runAndRenderTask = birtEngine.createRunAndRenderTask(report);
-        // response.setContentType(birtEngine.getMIMEType("pdf"));
-        IRenderOption options = new RenderOption();
-        PDFRenderOption pdfRenderOption = new PDFRenderOption(options);
+        PDFRenderOption pdfRenderOption = new PDFRenderOption();
         pdfRenderOption.setOutputFormat("pdf");
+        pdfRenderOption.setOutputFileName("cust.pdf");
         runAndRenderTask.setRenderOption(pdfRenderOption);
-        runAndRenderTask.getAppContext().put(EngineConstants.APPCONTEXT_PDF_RENDER_CONTEXT, this);
-        // FileOutputStream fout = null;
-        try (FileOutputStream fout = new FileOutputStream(new File("out.pdf"))){
-            // fout = new FileOutputStream(new File("out.pdf"));
-            pdfRenderOption.setOutputStream(fout);
-            runAndRenderTask.run();
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage(), e);
-        } finally {
-            runAndRenderTask.close();
-            // if (fout != null) {
-            //     try {
-            //         fout.close();
-            //     } catch (IOException ex) {
-            //         // TODO Auto-generated catch block
-            //         ex.printStackTrace();
-            //     }
-            // }
-        }
+        runAndRenderTask.run();
+        runAndRenderTask.close();
     }
-
-    // public List<Report> getReports() {
-    // List<Report> response = new ArrayList<>();
-    // for (Map.Entry<String, IReportRunnable> entry : reports.entrySet()) {
-    // IReportRunnable report = reports.get(entry.getKey());
-    // IGetParameterDefinitionTask task =
-    // birtEngine.createGetParameterDefinitionTask(report);
-    // Report reportItem = new
-    // Report(report.getDesignHandle().getProperty("title").toString(),
-    // entry.getKey());
-    // for (Object h : task.getParameterDefns(false)) {
-    // IParameterDefn def = (IParameterDefn) h;
-    // reportItem.getParameters()
-    // .add(new Report.Parameter(def.getPromptText(), def.getName(),
-    // getParameterType(def)));
-    // }
-    // response.add(reportItem);
-    // }
-    // return response;
-    // }
 }
